3bbe157669355d15f0eeb9c4d6ca4056e9bae28d
Front Coder <alex.mac.general@gmail.com>
2017-09-23 01:30:31 -0300

    fix package name

--- src/config.js
+++ src/config.js
@@ -1,10 +1,4 @@
-const TOKEN_API = 'BQC01U-c4xhYL1PThkD4emQSXQcqft4IX097TOCJ-cOzOkI7mc7loJkX-wTgujJOE0zbewC9fe78UDMs2W_hThFFr7QdkDs4u_4hA5UeNhT7mJac025Vhs70cw9jgb9V_BZ-JBL6iw';
-
 export const API_URL = 'https://api.spotify.com/v1';
 
-export const HEADERS = {
-  headers: {
-    Authorization: `Bearer ${TOKEN_API}`,
-  },
-};
+export default { API_URL };
 
